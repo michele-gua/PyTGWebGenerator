4e1f046e6f8ade15f630bfae128b2ea24afbbf2b
Michele Guarino <110943571+michele-gua@users.noreply.github.com>
2024-10-13 09:36:26 +0200

    First Commit
    Added
     - HTML Page
     - CSS Basic Styling
     - JS Functions
      > rangeToNumber to update the number field value based on the range one
      > numberToRange to update the range field based on the number one
      > ChatIDSubmit to submit the chatid if valid, else there will be an alert
      > containsOnlyNumbers to test if a string contains only numbers with regex

---
+++ js/script.js
@@ -0,0 +1,33 @@
+chatIDArray = [];
+document.getElementById("MSG_LIMIT_NUMBER").value = document.getElementById("MSG_LIMIT").value;
+
+
+const containsOnlyNumbers = (str) => {
+    return /^[0-9]+$/.test(str);
+};
+
+function ChatIDSubmit(){
+    if (containsOnlyNumbers(document.getElementById("chatids").value)){
+        chatID = document.getElementById("chatids").value;
+        if (chatID != ""){
+            chatIDArray.push(chatID);
+                txtarea = document.getElementById("Chat_IDS")
+                prima = txtarea.value;
+                if(!prima){
+                    txtarea.value = chatID;
+                } else {
+                    txtarea.value = prima += "," + chatID;
+                }   
+        }
+    }else{
+        alert("Not a valid value!");
+    }
+}
+
+function rangeToNumber(rangeObject, numberObject){
+    numberObject.value = rangeObject.value;
+}
+
+function numberToRange(rangeObject, numberObject){
+    rangeObject.value = numberObject.value;
+}
